eb11dc1a5a695932d94d6c5b087221b0807d8710
Steffen Schwigon <ss5@renormalist.net>
2015-09-22 16:12:04 +0200

    initial dist - CPAN deps for BenchmarkAnything

---
+++ lib/Task/BenchmarkAnything.pm
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+package Task::BenchmarkAnything;
+# ABSTRACT: Max dependencies for BenchmarkAnything
+
+=pkgroup benchmarkanything
+
+=pkg BenchmarkAnything::Schema
+
+=pkg BenchmarkAnything::Storage::Frontend::Tools
+
+=pkg BenchmarkAnything::Storage::Frontend::HTTP
+
+=pkg BenchmarkAnything::Storage::Frontend::Lib
+
+=pkg BenchmarkAnything::Storage::Backend::SQL
+
+1;
